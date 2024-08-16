7937577ec5e44bda68ed070b3b3708aec5a618b9
danielmilewski <milewski.daniel@gmail.com>
2024-08-16 18:01:47 +0200

    new shit

--- terraform/main.tf
+++ terraform/main.tf
@@ -7,6 +7,16 @@ terraform {
   }
 }
 
+
+terraform{
+ backend "remote"{
+  organization = "infra-test-90"
+  workspace{
+  name = "infra"
+  }
+ }
+}
+
 # Configure the DigitalOcean Provider
 provider "digitalocean" {
   token = var.do_token
